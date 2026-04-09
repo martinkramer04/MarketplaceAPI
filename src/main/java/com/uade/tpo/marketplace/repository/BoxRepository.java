f1dd9d23417c0f8b18ca04471e074fabb1e0bb4d
martinkramer04 <martinkramer04@gmail.com>
2026-04-09 19:08:07 -0300

    Agregue box

---
+++ src/main/java/com/uade/tpo/marketplace/repository/BoxRepository.java
@@ -0,0 +1,16 @@
+package com.uade.tpo.marketplace.repository;
+ 
+import java.util.List;
+ 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+ 
+import com.uade.tpo.marketplace.entity.Box;
+ 
+@Repository
+public interface BoxRepository extends JpaRepository<Box, Long> {
+ 
+    List<Box> findByCategoryId(Long categoryId);
+ 
+    List<Box> findByStockGreaterThan(Integer stock);
+}
